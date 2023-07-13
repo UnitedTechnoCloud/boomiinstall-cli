50229fb60a81132ae1f9b74c440a742b0e79ad8d
Srini <srinivasula.y@gmail.com>
2023-07-13 18:41:57 +0530

    new boomi installation script file

--- cli/scripts/bin/boomi_runtime_installer_old.sh
+++ cli/scripts/bin/boomi_runtime_installer_old.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 #set -x
-if [ -n ${platform} ] ; then
-    if [[ -f /etc/boomi_runtime_installer ]]; then 
-        echo "boomi_runtime_installer already run so will not be run again!"
-        exit 0; 
-    fi
-fi
-
-echo "begin boomi install..."
 USR=boomi
 GRP=boomi
 whoami
-echo "Cloud Platform : ${platform}"
-echo "Atom Name : ${atomName}"
-echo "Atom Type : ${atomType}"
-echo "Boomi Environment : ${boomiEnv}"
-echo "purge Days : ${purgeHistoryDays}"
-echo "max Memory : ${maxMem}"
-echo "efsMount : ${efsMount}"
-echo "githubToken : ${githubToken}"
+echo "Cloud Platform is: ${platform}"
+echo "Atom Name is: ${atomName}"
+echo "Atom Type is: ${atomType}"
 
 #  create boomi user
 sudo groupadd -g 5151 -r $GRP
@@ -26,30 +13,31 @@ sudo useradd -u 5151 -g $GRP -r -m -s /bin/bash $USR
 sudo usermod -aG sudo boomi
 echo "boomi ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 sudo apt-get -y update
-echo "install python..."
-sudo apt-get install -y zip -y
-sudo apt-get install python3-pip -y
+sudo apt-get install -y zip
+sudo apt-get install python3-pip
 python3 --version
-sudo apt-get install -y ca-certificates curl gnupg  lsb-release -y
+sudo apt-get install -y ca-certificates curl gnupg  lsb-release
 
 # set ulimits
 sudo sysctl -w net.core.rmem_max=8388608
 sudo sysctl -w net.core.wmem_max=8388608
 sudo sysctl -w net.core.rmem_default=65536
 sudo sysctl -w net.core.wmem_default=65536
+
+sudo iptables -A INPUT -p tcp --dport 7800 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 5003 -j ACCEPT
 printf "%s\t\t%s\t\t%s\t\t%s\n" $USR "soft" "nproc" "65535" | sudo tee -a /etc/security/limits.conf
 printf "%s\t\t%s\t\t%s\t\t%s\n" $USR "hard" "nproc" "65535" | sudo tee -a /etc/security/limits.conf
 printf "%s\t\t%s\t\t%s\t\t%s\n" $USR "soft" "nofile" "8192" | sudo tee -a /etc/security/limits.conf
 printf "%s\t\t%s\t\t%s\t\t%s\n" $USR "hard" "nofile" "8192" | sudo tee -a /etc/security/limits.conf
 
 # install java
-echo "install java..."
-sudo apt-get update && sudo apt-get install -y java-common -y
+sudo apt-get update && sudo apt-get install -y java-common
 curl -fssL https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.deb -o amazon-corretto-11-x64-linux-jdk.deb
 sudo dpkg --install amazon-corretto-11-x64-linux-jdk.deb
 cd /usr/lib/jvm/
 sudo ln -sf java-11-amazon-corretto/ jre
-sudo apt-get -y install git binutils -y
 
 if [ "${platform}" = "aws" ]; then
     sudo apt-get install -y awscli
@@ -63,20 +51,16 @@ else
     echo "awscli install not required!"
 fi
 
-set -e
 ## download boomicicd CLI 
-sudo apt-get install -y jq -y
-sudo apt-get install -y libxml2-utils -y
+sudo apt-get install -y jq
+sudo apt-get install -y libxml2-utils
 mkdir -p  /home/$USR/boomi/boomicicd
 cd /home/$USR/boomi/boomicicd
-echo "git clone https://github.com/UnitedTechnoCloud/boomiinstall-cli..."
-#git clone https://${githubToken}@github.com/UnitedTechnoCloud/boomicicd-cli
-git clone https://github.com/UnitedTechnoCloud/boomiinstall-cli
+#git clone https://${GitUserName}:${GitPAT}/UnitedTechnoCloud/boomicicd-cli.git
+git clone https://github.com/TommyJohnWear/boomicicd-cli
 cd /home/$USR/boomi/boomicicd/boomicicd-cli/cli/
-set +e
 
 # download Boomi installers
-echo "download boomi installers..."
 curl -fsSL https://platform.boomi.com/atom/atom_install64.sh -o atom_install64.sh && chmod +x "atom_install64.sh"
 curl -fsSL https://platform.boomi.com/atom/molecule_install64.sh -o molecule_install64.sh && chmod +x "molecule_install64.sh"
 curl -fsSL https://platform.boomi.com/atom/cloud_install64.sh -o cloud_install64.sh && chmod +x "cloud_install64.sh"
@@ -95,6 +79,7 @@ chmod u+x /home/$USR/.profile
 echo "if [ -f /home/$USR/.profile ]; then" >> /home/$USR/.bashrc
 echo "	. /home/$USR/.profile" >> /home/$USR/.bashrc
 echo "fi" >> /home/$USR/.bashrc
+
 cp /home/$USR/boomi/boomicicd/boomicicd-cli/cli/scripts/home/.profile .
 if [ "${platform}" = "aws" ]; then
     EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
@@ -103,6 +88,8 @@ if [ "${platform}" = "aws" ]; then
     echo "export AWS_DEFAULT_PROFILE=boomi-profile" >> .profile
     echo "export AWS_DEFAULT_REGION=$EC2_REGION" >> .profile 
     source /home/$USR/.profile
+else
+    echo "awscli install not required!"
 fi
             
 # set up local directories for install
@@ -115,10 +102,7 @@ chown -R $USR:$GRP /home/$USR/
 chown -R $USR:$GRP /usr/local/boomi/
 chown -R $USR:$GRP /usr/local/bin/
 whoami
-
-# install boomi
 sudo -u $USR bash << EOF
-echo "install boomi runtime as $USR"
 if [ "${platform}" = "aws" ]; then
     echo "Hello, I am running as $USR"
     source /home/$USR/.profile
@@ -143,20 +127,9 @@ if [ "${platform}" = "aws" ]; then
 	#export authToken="BOOMI_TOKEN.$userName:$apiToken"
 	export authToken=${authToken}
     source bin/exports.sh
+	env
     source bin/init.sh atomType=${atomType} atomName=${atomName} env=${env} classification=${classification} accountId=${accountId}	purgeHistoryDays=${purgeHistoryDays} maxMem=${maxMem} defaultRegion=${defaultRegion}
 else
-    # GCP/Azure platforms
-    cd /home/$USR/boomi/boomicicd/boomicicd-cli/cli/scripts
-    source bin/exports.sh
-    source bin/efsMount.sh efsMount=${efsMount}
-    echo "run init.sh..."
-    sudo su - $USR -c "cd ~/boomi/boomicicd/boomicicd-cli/cli/scripts;export authToken=${boomiAtmosphereToken};. bin/init.sh atomType=${atomType} atomName=${atomName} env=${boomiEnv} classification=${boomiClassification} accountId=${boomiAccountId} purgeHistoryDays=${purgeHistoryDays} maxMem=${maxMem}"
+    echo "awscli install not required!"
 fi
 EOF
-
-echo "boomi install complete..."
-
-if [ -n ${platform} ] ; then
-  touch /etc/boomi_runtime_installer
-  echo "boomi_runtime_installer flag created"
-fi
