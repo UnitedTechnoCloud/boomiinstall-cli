f80b0147ae935b47e3df61fc5ea6aca0ab91237b
Srini <srinivasula.y@gmail.com>
2023-07-26 17:19:58 +0530

    adding for aws

--- cli/scripts/bin/boomi_runtime_installer.sh
+++ cli/scripts/bin/boomi_runtime_installer.sh
@@ -18,6 +18,11 @@ echo "Boomi Environment : ${boomiEnv}"
 echo "purge Days : ${purgeHistoryDays}"
 echo "max Memory : ${maxMem}"
 echo "efsMount : ${efsMount}"
+echo "githubToken : ${githubToken}"
+echo "client : ${client}"
+echo "group : ${group}"
+echo "region : ${region}"
+
 
 #  create boomi user
 sudo groupadd -g 5151 -r $GRP
@@ -124,7 +129,8 @@ if [ -n "$efsMount" ] ; then
 fi
 export authToken=${boomiAtmosphereToken}
 echo "run init.sh..."
-. bin/init.sh atomType=${atomType} atomName=${atomName} env=${boomiEnv} classification=${boomiClassification} accountId=${boomiAccountId} purgeHistoryDays=${purgeHistoryDays} maxMem=${maxMem}
+env
+. bin/init.sh atomType=${atomType} atomName=${atomName} env=${boomiEnv} classification=${boomiClassification} accountId=${boomiAccountId} purgeHistoryDays=${purgeHistoryDays} maxMem=${maxMem} defaultRegion=${region}
 EOF
 
 echo "boomi install complete..."
