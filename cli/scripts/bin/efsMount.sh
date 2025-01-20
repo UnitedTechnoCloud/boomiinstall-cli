8423240cf01ad41e9351bc0157382c7b2156fdaf
srinivasulary <128830105+srinivasulary@users.noreply.github.com>
2025-01-20 21:26:40 +0530

    Update efsMount.sh

--- cli/scripts/bin/efsMount.sh
+++ cli/scripts/bin/efsMount.sh
@@ -44,4 +44,4 @@ else
 	echo "${efsMount} $mountPoint nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab
 fi
 sudo mount -a
-# sudo chown -R $serviceUserName:$groupName "${mountPoint}"
+sudo chown -R $serviceUserName:$groupName "${mountPoint}"
