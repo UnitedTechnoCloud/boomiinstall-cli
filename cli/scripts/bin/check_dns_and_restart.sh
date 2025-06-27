cd0f50acedc552429c5635f59435ecfc13047d82
Srini <srinivasula.y@gmail.com>
2025-06-27 12:58:24 +0530

    refactoring

--- cli/scripts/bin/check_dns_and_restart.sh
+++ cli/scripts/bin/check_dns_and_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CONFIGURATION
-DOMAINS=("flow-sql-prod-01.database.windows.net" "flow-sql-test-01.database.windows.net" "AZR-SVR-APP-D01.sanimax.int" "GRB-OT-SQL-P01.sanimax.int")  # List of DNS names
+DOMAINS=("RDBMS9SRVBELL" "flow-sql-test-01" "BIDSQL1N1" "AZR-SVR-APP-D01","AZR-SVR-SQL-P02","flow-sql-prod-01","GRB-OT-SQL-P01")  # List of DNS names
 SNS_TOPIC_ARN="arn:aws:sns:ca-central-1:456486178888:boomi-sanimax-ATOM-dev-runtime-stack-NotificationTopic-0l1lG1GjuUNR"
 AWS_REGION="ca-central-1"
 HOSTNAME=$(hostname)
@@ -35,16 +35,16 @@ while true; do
 
 	  if is_private_ip "$resolved_ip"; then
 		  echo "$(date): $domain resolved to private IP."
-		  aws sns publish --region "$AWS_REGION" --topic-arn "$SNS_TOPIC_ARN" --subject "DNS Warning on $HOSTNAME" --message "$DNS_NAME Resolved to PUBLIC IP: $resolved_ip."			
+		  aws sns publish --region "$AWS_REGION" --topic-arn "$SNS_TOPIC_ARN" --subject "DNS Warning on $HOSTNAME" --message "$DNS_NAME Resolved to Private IP: $resolved_ip."		
 	  else
-		  echo "$(date): $domain resolved to PUBLIC IP ($resolved_ip). Restarting systemd-resolved.."
+		  echo "$(date): $domain not resolved. Restarting systemd-resolved.."
 		  all_private=false
 
 		  # Restart systemd-resolved
 		  sudo systemctl restart systemd-resolved
 
 		  # Send SNS notification
-		  aws sns publish --region "$AWS_REGION" --topic-arn "$SNS_TOPIC_ARN" --subject "DNS Warning on $HOSTNAME" --message "$DNS_NAME Resolved to PUBLIC IP: $resolved_ip."
+		  aws sns publish --region "$AWS_REGION" --topic-arn "$SNS_TOPIC_ARN" --subject "DNS Warning on $HOSTNAME" --message "$DNS_NAME is not Resolved to Private IP: $resolved_ip."
 		  # Sleep before checking again
 		  sleep "$RETRY_INTERVAL"
 	  fi
