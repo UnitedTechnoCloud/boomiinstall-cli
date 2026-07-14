e1f50a020976115d5a810ca8c3d85447bd7ff65c
Srinivasu <srinivasular@unitedtechno.com>
2026-07-14 03:04:36 +0000

    fix: use bin/xxx_install64.sh path since CWD is cli/scripts/ at runtime

--- cli/scripts/bin/installCloud.sh
+++ cli/scripts/bin/installCloud.sh
@@ -66,7 +66,7 @@ then
  proxyParams="${proxyParams} -VproxyPassword='${proxyPassword}'"
 fi
 
-./cloud_install64.sh -q -console  \
+bin/cloud_install64.sh -q -console  \
 -VinstallToken=$tokenId \
 -VatomName=$atomName \
 -VlocalTempPath=${TMP_DIR} \
--- cli/scripts/bin/installMolecule.sh
+++ cli/scripts/bin/installMolecule.sh
@@ -63,7 +63,7 @@ then
 	proxyParams="${proxyParams} -VproxyPassword='${proxyPassword}'"
 fi
 
-./molecule_install64.sh -q -console  \
+bin/molecule_install64.sh -q -console  \
 -VinstallToken=$tokenId \
 -VatomName=$atomName \
 -VlocalTempPath=${TMP_DIR} \
